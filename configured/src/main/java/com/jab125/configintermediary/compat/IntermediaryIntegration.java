d1b94642a3b597d1dfb84bcf53529fedfc8bd61b
Jab125 <67534807+Jab125@users.noreply.github.com>
2023-01-02 20:25:01 +1100

    Update IntermediaryIntegration.java

--- configured/src/main/java/com/jab125/configintermediary/compat/IntermediaryIntegration.java
+++ configured/src/main/java/com/jab125/configintermediary/compat/IntermediaryIntegration.java
@@ -10,28 +10,19 @@ import com.mrcrayfish.configured.client.ClientHandler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class IntermediaryIntegration implements ModInitializer, ConfigRegistration {
-    private Map<String, Set<IModConfig>> configs;
+    private Map<String, Set<IModConfig>> configs = new HashMap<>();
     @Override
     public void onConfigRegistered(Config config) {
-
+        configs.put(config.getId(), Set.of(new IntermediaryConfig(config)));
     }
 
     @Override
     public void onInitialize() {
-        ClientHandler.getProviders().add(modContainer -> {
-            if (configs == null) {
-                var l = new ImmutableMap.Builder<String, Set<IModConfig>>();
-                ConfigIntermediary.configs.forEach((a, b) -> {
-                    if (FabricLoader.getInstance().getModContainer(a).isEmpty()) return;
-                    l.put(FabricLoader.getInstance().getModContainer(a).get().getMetadata().getId(), Set.of(new IntermediaryConfig(b)));
-                });
-                configs = l.build();
-            }
-            return configs.getOrDefault(modContainer.getMetadata().getId(), Set.of());
-        });
+        ClientHandler.getProviders().add(modContainer -> configs.getOrDefault(modContainer.getMetadata().getId(), Set.of()));
     }
 }
